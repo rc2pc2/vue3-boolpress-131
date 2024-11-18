59b1692484f173e2ebfe7cf3dafbd0afa81bf451
rc2pc2 <petriccarcc@gmail.com>
2024-11-18 11:48:44 +0100

    added pagination behaviour

--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -12,29 +12,61 @@ export default {
     data() {
         return {
             postList : [],
-            apiUrl: "http://127.0.0.1:8000/api/post s",
+            apiUrl: "http://127.0.0.1:8000/api/posts",
             // loaded: false,
+            lastPageNumber : 1,
+            currentPageNumber: 1,
+
+
         }
     },
     methods:{
-        getPosts(){
-            axios.get(this.apiUrl)
+        getPosts(pageNumber){
+            axios.get(this.apiUrl, {
+                    params: {
+                        page: pageNumber
+                    }
+                })
                 .then((response) => {
-                    console.log(response.data.results);
-                    this.postList = response.data.results;
+                    console.log(response.data.results.data);
+                    console.log(pageNumber);
+                    this.postList = response.data.results.data;
+                    this.lastPageNumber = response.data.results.last_page;
+                    this.currentPageNumber = pageNumber;
                     // this.loaded = true;
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
+        },
+        previousPage(){
+            console.log("Previous click");
+            if(this.currentPageNumber > 1){
+                this.currentPageNumber--;
+                this.getPosts(this.currentPageNumber);
+            }
+        },
+        nextPage(){
+            console.log("Next click");
+            if(this.currentPageNumber < this.lastPageNumber){
+                this.currentPageNumber++; // 3
+                this.getPosts(this.currentPageNumber);
+            }
         }
+
     },
     created(){
-        this.getPosts();
+        this.getPosts(1);
     },
     computed:{
         loaded(){
             return this.postList.length > 0;
+        },
+        firstPage(){
+            return this.currentPageNumber === 1;
+        },
+        lastPage(){
+            return this.currentPageNumber >= this.lastPageNumber;
         }
     }
 }
@@ -50,6 +82,28 @@ export default {
         <PostsListCard v-for="singlePost in postList" 
             :key="singlePost.id" 
             :singlePostObject="singlePost" />
+    </section>
+
+    <section class="navigation">
+        <nav>
+            <ul class="d-flex justify-content-between list-unstyled">
+                <li>
+                    <button class="btn btn-primary " @click="previousPage"
+                        :class="{ 'disabled' : firstPage }"
+                    >
+                        Prev
+                    </button>
+                </li>
+                
+                <li>
+                    <button class="btn btn-primary" @click="nextPage"
+                        :class="{ 'disabled' : lastPage }" 
+                    >
+                        Next
+                    </button>
+                </li>
+            </ul>
+        </nav>
     </section>
 </template>
 
